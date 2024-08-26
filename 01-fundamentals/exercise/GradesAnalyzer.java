38814f5adb7d84553c1ba8de65222a3ed5bad3e0
narcisolobo <narcisolobo@gmail.com>
2024-08-26 14:27:43 -0700

    w10d1 afternoon

--- 01-fundamentals/exercise/GradesAnalyzer.java
+++ 01-fundamentals/exercise/GradesAnalyzer.java
@@ -1,5 +1,4 @@
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
 
 public class GradesAnalyzer {
 
@@ -10,8 +9,8 @@ public class GradesAnalyzer {
         System.out.print("How many grades would you like to input? ");
         int numGrades = scanner.nextInt();
 
-        for (int i = 0; i < numGrades; i++) {
-            System.out.print("Enter grade " + (i + 1) + ": ");
+        for (int i = 1; i <= numGrades; i++) {
+            System.out.print("Enter grade " + i + ": ");
             int grade = scanner.nextInt();
             grades.add(grade);
         }
@@ -19,5 +18,60 @@ public class GradesAnalyzer {
         System.out.println("Grades entered: " + grades);
         scanner.close();
         return grades;
+    }
+
+    public int findHighest(ArrayList<Integer> grades) {
+        int highest = Collections.max(grades);
+        return highest;
+    }
+
+    public int findLowest(ArrayList<Integer> grades) {
+        int lowest = Collections.min(grades);
+        return lowest;
+    }
+
+    public double findAverage(ArrayList<Integer> grades) {
+        int sum = 0;
+        int size = grades.size();
+
+        // for (int i = 0; i < size; i++) {
+        //     sum += grades.get(i);
+        // }
+
+        // enhanced for loop
+        for (int grade : grades) {
+            sum += grade;
+        }
+
+        return sum / size;
+    }
+
+    public int findNumberPassing(ArrayList<Integer> grades) {
+        int count = 0;
+        for (int grade : grades) {
+            if (grade >= 60) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public boolean overallPassed(double average) {
+        return average >= 60;
+    }
+
+    public String performance(double average) {
+        if (average >= 90) {
+            return "Excellent";
+        } else if (average >= 80) {
+            return "Good";
+        } else if (average >= 70) {
+            return "Average";
+        } else if (average >= 60) {
+            return "Passable";
+        } else {
+            return "Failing";
+        }
     }
 }
--- 01-fundamentals/exercise/TestGradesAnalyzer.java
+++ 01-fundamentals/exercise/TestGradesAnalyzer.java
@@ -4,5 +4,29 @@ public class TestGradesAnalyzer {
     public static void main(String[] args) {
         GradesAnalyzer analyzer = new GradesAnalyzer();
         ArrayList<Integer> grades = analyzer.promptForGrades();
+
+        int highest = analyzer.findHighest(grades);
+        System.out.printf("The highest grade is: %d.\n", highest);
+        String message = highest >= 90 ? "Great job!" : "Keep trying!";
+        System.out.println(message);
+
+        int lowest = analyzer.findLowest(grades);
+        System.out.printf("The lowest grade is: %d.\n", lowest);
+
+        double average = analyzer.findAverage(grades);
+        System.out.printf("The average grade is: %.2f.\n", average);
+
+        int numPassing = analyzer.findNumberPassing(grades);
+        System.out.printf("The number of passing students is: %d.\n", numPassing);
+
+        boolean overallPassed = analyzer.overallPassed(average);
+        if (overallPassed) {
+            System.out.println("The class passed!");
+        } else {
+            System.out.println("The class did not pass.");
+        }
+
+        String performance = analyzer.performance(average);
+        System.out.println(performance);
     }
 }
