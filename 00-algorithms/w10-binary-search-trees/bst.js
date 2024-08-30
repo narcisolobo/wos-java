b44c26afc1bc1fe661c90d535931d14755bcec32
narcisolobo <narcisolobo@gmail.com>
2024-08-30 07:11:41 -0700

    w10d5 algo prep

--- 00-algorithms/w10-binary-search-trees/bst.js
+++ 00-algorithms/w10-binary-search-trees/bst.js
@@ -315,10 +315,49 @@ class BinarySearchTree {
     }
     return vals;
   }
+
+  /**
+   * BFS order: horizontal rows top-down left-to-right.
+   * Converts this BST into an array following Breadth First Search order.
+   * Example on the fullTree var:
+   * [25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90]
+   * @param {Node} current The current node during the traversal of this tree.
+   * @returns {Array<number>} The data of all nodes in BFS order.
+   */
+  toArrLevelorder(current = this.root) {}
+
+  /**
+   * Calculates the height of the tree which is based on how many nodes from
+   * top to bottom (whichever side is taller).
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Node} node The current node during traversal of this tree.
+   * @returns {number} The height of the tree.
+   */
+  height(node = this.root) {}
+
+  /**
+   * Recursively counts the total number of nodes in this tree.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Node} node The current node during the traversal of this tree.
+   * @returns {number} The total number of nodes.
+   */
+  size(node = this.root) {}
+
+  /**
+   * Determines if this tree is a full tree. A full tree is a tree where every
+   * node has both a left and a right except for the leaf nodes (last nodes)
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Node} node The current node during traversal of this tree.
+   * @returns {boolean} Indicates if this tree is full.
+   */
+  isFull(node = this.root) {}
 }
 
 const emptyTree = new BinarySearchTree();
-/* oneLevelTree
+/* oneNodeTree
         root
         10
 */
@@ -382,5 +421,33 @@ fullTree
   .insert(66)
   .insert(90);
 
-// oneNodeTree.print();
-// twoLevelTree.print();
+const result1 = emptyTree.toArrLevelorder();
+const expected1 = [];
+console.log(`${result1} should equal ${expected1}`);
+
+const result2 = oneNodeTree.toArrLevelorder();
+const expected2 = [10];
+console.log(`${result2} should equal ${expected2}`);
+
+const result3 = twoLevelTree.toArrLevelorder();
+const expected3 = [10, 5, 15];
+console.log(`${result3} should equal ${expected3}`);
+
+const result4 = threeLevelTree.toArrLevelorder();
+const expected4 = [10, 5, 15, 2, 6, 13];
+console.log(`${result4} should equal ${expected4}`);
+
+const result5 = fullTree.toArrLevelorder();
+const expected5 = [25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90];
+
+const result6 = fullTree.height();
+const expected6 = 4;
+console.log(`${result6} should equal ${expected6}`);
+
+const result7 = fullTree.size();
+const expected7 = 15;
+console.log(`${result7} should equal ${expected7}`);
+
+const result8 = fullTree.isFull();
+const expected8 = true;
+console.log(`${result8} should equal ${expected8}`);
