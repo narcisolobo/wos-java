f8744800f6b946be41e1e5c6ead6393e8a48ad85
narcisolobo <narcisolobo@gmail.com>
2024-09-05 09:37:59 -0700

    w11d4 morning

--- 00-algorithms/w11-stacks-and-queues/queue.js
+++ 00-algorithms/w11-stacks-and-queues/queue.js
@@ -66,6 +66,32 @@ class Queue {
   size() {
     return this.items.length;
   }
+
+  /**
+   * Compares this queue to another given queue to see if they are equal.
+   * Avoid indexing the queue items directly via bracket notation, use the
+   * queue methods instead for practice.
+   * Use no extra array or objects.
+   * The queues should be returned to their original order when done.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Queue} q2 The queue to be compared against this queue.
+   * @returns {boolean} Whether all the items of the two queues are equal and
+   *    in the same order.
+   */
+  compareQueues(q2) {}
+
+  /**
+   * Determines if the queue is a palindrome (same items forward and backwards).
+   * Avoid indexing queue items directly via bracket notation, instead use the
+   * queue methods for practice.
+   * Use only 1 stack as additional storage, no other arrays or objects.
+   * The queue should be returned to its original order when done.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @returns {boolean}
+   */
+  isPalindrome() {}
 }
 
 const emptyQueue = new Queue();
