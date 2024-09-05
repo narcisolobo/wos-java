7f8127b3a9a3fdb0527b4c2ea409b8ceee970148
narcisolobo <narcisolobo@gmail.com>
2024-09-05 06:47:04 -0700

    w11d4 algo prep

---
+++ 00-algorithms/w11-stacks-and-queues/queue.js
@@ -0,0 +1,54 @@
+/**
+ * Class to represent a queue using an array to store the queued items.
+ * Follows a FIFO (First In First Out) order where new items are added to the
+ * back and items are removed from the front.
+ */
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  /**
+   * Adds a new given item to the back of this queue.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @param {any} item The new item to add to the back.
+   * @returns {number} The new size of this queue.
+   */
+  enqueue(item) {}
+
+  /**
+   * Removes and returns the first item of this queue.
+   * - Time: O(n) linear, due to having to shift all the remaining items to
+   *    the left after removing first elem.
+   * - Space: O(1) constant.
+   * @returns {any} The first item or undefined if empty.
+   */
+  dequeue() {}
+
+  /**
+   * Retrieves the first item without removing it.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {any} The first item or undefined if empty.
+   */
+  front() {}
+
+  /**
+   * Returns whether or not this queue is empty.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {boolean}
+   */
+  isEmpty() {}
+
+  /**
+   * Retrieves the size of this queue.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {number} The length.
+   */
+  size() {}
+}
+
+/* Rebuild the above class using a linked list instead of an array. */
